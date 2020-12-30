9ba45ec86e6cb3afbd14604d89203eb7139fed0d
Samuel Oechsler <samuel@oechsler.it>
2020-12-30 14:06:58 +0100

    Fix uninstall wkatime cfg not removed

--- scripts/zsh.zsh
+++ scripts/zsh.zsh
@@ -111,7 +111,7 @@ remove_zsh() {
     sudo rm -r $HOME/.oh-my-zsh/
     rm $HOME/.zshrc
     mv $HOME/.zshrc.pre-dotfiles $HOME/.zshrc $HOME/.wakatime.cfg
-    rm $PWD/$USER.zshrc $PWD/$SUER.wakatime.cfg
+    rm $PWD/$USER.zshrc $PWD/$USER.wakatime.cfg
 
     write_line ${RED} "Removed ${RED}zsh${RBOLD}."
 }
